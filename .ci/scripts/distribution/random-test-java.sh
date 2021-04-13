a1507c8db40e365cf8043f2d91fe56a1b0316abe
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-04-13 09:17:06 +0200

    ci: add missing temp file to collect output

--- .ci/scripts/distribution/random-test-java.sh
+++ .ci/scripts/distribution/random-test-java.sh
@@ -2,6 +2,7 @@
 
 # specialized script to run tests with random elements without flaky test detection
 # no need for special parallelism parameters since we only run a single test
+tmpfile=$(mktemp)
 mvn -o -B --fail-never -T${MAVEN_PARALLELISM} -s ${MAVEN_SETTINGS_XML} \
     -P skip-unstable-ci,include-random-tests \
     "${MAVEN_PROPERTIES[@]}" verify | tee ${tmpfile}
